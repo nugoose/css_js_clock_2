4eeaaa892e630e93448e0d75bf2dc0d2cc8333e5
nugoose <nugoose@gmail.com>
2017-01-15 00:23:27 -0500

    updated js mostly
    working hands but not 100% ready

--- css_js_clock.js
+++ css_js_clock.js
@@ -4,4 +4,23 @@ var domReady = function(callback) {
 
 domReady(function() {
     
+    setInterval(function(){
+        var t = new Date(),
+        h = t.getHours(),
+        m = t.getMinutes(),
+        s = t.getSeconds(),
+        hDeg = 'rotate(' + ((h % 12)*30) + 'deg)',
+        mDeg = 'rotate(' + (m * 6) + 'deg)',
+        sDeg = 'rotate(' + (s * 6) + 'deg)';
+    
+        console.log(h,m,s);
+    
+        /////hours////
+        document.getElementById('hoursHand').style.transform = hDeg;
+        ////minutes////
+        document.getElementById('minutesHand').style.transform = mDeg;
+        /////seconds/////
+        document.getElementById('secondsHand').style.transform = sDeg;
+    }, 1000);
+    
 });
